e6a31727a38602bf7356d783b3a72212c06d34d7
Roman <kasyan.ua@gmail.com>
2025-01-23 00:30:53 +0200

    final changes in course

--- src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -1,5 +1,7 @@
 package com.amigoscode;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,8 +11,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringAndSpringBootApplication {
@@ -25,7 +33,7 @@ public class SpringAndSpringBootApplication {
         System.out.println(beanDefinitionCount);
     }
 
-//    @Bean(name = "red-bean")
+    //    @Bean(name = "red-bean")
     @Bean()
 //    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 //    @SessionScope
@@ -42,12 +50,45 @@ public class SpringAndSpringBootApplication {
     @Bean
     CommandLineRunner commandLineRunner(
 //            @Qualifier("red-bean")
-            String redBean, String blueBean) {
+            String redBean, String blueBean, UserService userService) {
         return args -> {
             System.out.println("Hello World");
             System.out.println(redBean);
             System.out.println(blueBean);
+            System.out.println(userService.getUserByID(1));
         };
     }
 
+    public record User(int id, String name) {
+    }
+
+    @Service
+    public class UserService {
+
+        public UserService() {
+            System.out.println("UserService constructor");
+        }
+
+        @PostConstruct
+        public void init(){
+            System.out.println("init, fill cache");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("destroy cache");
+        }
+
+        @Bean
+        public List<User> getUsers() {
+            return List.of(
+                    new User(1, "John Doe"),
+                    new User(2, "Jane Doe"),
+                    new User(3, "Jack Doe"));
+        }
+
+        public Optional<User> getUserByID(int id) {
+            return getUsers().stream().filter(user -> user.id == id).findFirst();
+        }
+    }
 }
