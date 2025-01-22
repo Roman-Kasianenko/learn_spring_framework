3bb6327d3c389c3f1f7a971ca8e65c544e4608f4
Roman <kasyan.ua@gmail.com>
2025-01-22 15:23:15 +0200

    added first text bean :)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# learn_spring_framework
+# Basic Spring boot training
+
+Start checking spring boot courses to recall knowledge,
+since was not using Spring Boot for a while.
+
+Let's start from the beginning 😛
+
+This one: https://www.amigoscode.com/learn/spring-framework
--- src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -1,16 +1,53 @@
 package com.amigoscode;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @SpringBootApplication
 public class SpringAndSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(
+        ConfigurableApplicationContext context = SpringApplication.run(
                 SpringAndSpringBootApplication.class,
                 args
         );
+
+        int beanDefinitionCount = context.getBeanDefinitionCount();
+        System.out.println(beanDefinitionCount);
+    }
+
+//    @Bean(name = "red-bean")
+    @Bean()
+//    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//    @SessionScope
+//    @ApplicationScope
+    public String redBean() {
+        return "from redBean";
+    }
+
+    @Bean
+    public String blueBean() {
+        return "from blueBean";
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+//            @Qualifier("red-bean")
+            String redBean, String blueBean) {
+        return args -> {
+            System.out.println("Hello World");
+            System.out.println(redBean);
+            System.out.println(blueBean);
+        };
     }
 
 }
